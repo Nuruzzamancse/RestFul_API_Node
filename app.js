3bbc3367879e6036c6b1274e18d014b8c5c54d09
Nuruzzamancse <mr.nuruzzamancse@gmail.com>
2017-12-19 12:41:41 +0600

    CORS handling

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const productsRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
 
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
 app.use('/products',productsRoutes);
 app.use('/orders',orderRoutes);
-app.use(morgan('dev'));
+
+app.use((req,res,next)=>{
+    
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Headers',
+    "Origin, X-Requested-With, Content-Type,Accept,Authorization");
+
+    if(req.method==='OPTIONS'){
+        res.header('Access-Control-Allow-Methods','PUT POST PATH DELETE');
+        return res.status(200).json({});
+    }
+    next();
+
+})
+
 
 app.use((req,res,next)=>{
     const error = new Error('Not found');
